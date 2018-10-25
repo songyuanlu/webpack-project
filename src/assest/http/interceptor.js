ad47bb34a6b2e7e972e1e5a87ad8fc9977d0f02c
songyuanlu <syl123456>
2018-10-25 13:55:34 +0800

    优化请求拦截器

--- src/assest/http/interceptor.js
+++ src/assest/http/interceptor.js
@@ -1,19 +1,98 @@
 import Vue from 'vue';
 
-const interceptor = function(){
+const interceptor = function () {
+  /** http 设置全局配置 **/
+  // Vue.http.options.root = 'http://127.0.0.1:8081/';
+
+  //请求发送前的处理逻辑
+  Vue.http.interceptors.push(function (request, next) {
+
+    // 请求发送前的处理逻辑
+    request = Object.assign({
+      method: 'POST',
+      timeout: 10000,
+      headers: {
+        'Accept': 'application/json,text/json',
+        'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
+      },
+      showLoading: true,
+    }, request);
+    console.log(request)
+
+    //发送请求前加显示loading
+    if (request.showLoading === true) {
+      Vue.$loading.show({
+        show: true,
+        msg: "拦截器的加载"
+      });
+      Vue.$loading.add();
+    }
+
+    next(function (response) {
+      //发送请求后隐藏loading
+      if (request.showLoading === true) {
+        Vue.$loading.minus();
+        console.log(Vue.$loading.amount);
+        if (Vue.$loading.amount === 0) {
+          Vue.$loading.hide();
+        }
+      }
+      return response
+    })
+  });
+
+  //请求完成的-断网处理
+  Vue.http.interceptors.push(function (request, next) {
+    next(function (response) {
+      if (response.status === 0) {
+        console.log('断网了');
+      }
+      return response
+    })
+  });
+
+  //请求完成的-404处理
+  Vue.http.interceptors.push(function (request, next) {
+    next(function (response) {
+      if (response.status === 404) {
+        console.log('404了');
+      }
+      return response
+    })
+  });
+
+  //请求完成的-异常处理
+  Vue.http.interceptors.push(function (request, next) {
+    next(function (response) {
+      if (response.status === 200)
+        switch (+response.body.resultCode) {
+          case 0:
+            console.log('错误了')
+            break
+
+          case 9999:
+            console.log('登录超时了')
+            break
+        }
+
+      return response
+    })
+  });
+
+}
+
+export default interceptor;
 
 
-/** http 设置全局配置 **/
-
+/** 请求拦截demo ******/
   //设置请求域
   // Vue.http.options.root = 'http://127.0.0.1:8081/';
-  Vue.http.options.root = '';
   //如果请求是基于这个域
   //错误: this.$http.get("/list.json"），不然是基于页面域名的请求
   //正确： this.$http.get("list.json"）
 
   //设置请求头
-  Vue.http.headers.common['Authorization'] = 'Basic YXBpOnBhc3N3b3Jk';
+  // Vue.http.headers.common['Authorization'] = 'Basic YXBpOnBhc3N3b3Jk';
 
   //Vue组件内设置默认值选项。
   // new Vue({
@@ -25,123 +104,24 @@ const interceptor = function(){
   //     }
   // })
 
-  /** 拦截器 **/
-  Vue.http.interceptors.push(function (request, next) {
-    // ...
-    // 请求发送前的处理逻辑
-    // ...
-    Vue.$loading.show({
-        show: true,
-        msg: "拦截器的加载"
-      });
-    console.log('request-1111')
+  // Vue.http.interceptors.push(function (request, next) {
+  //   // modify method
+  //   request.method = 'POST';
+  //   // modify headers
+  //   request.headers.set('X-CSRF-TOKEN', 'TOKEN');
 
-    // modify method
-    request.method = 'POST';
-    // modify headers
-    request.headers.set('X-CSRF-TOKEN', 'TOKEN');
+  //   //直接返回，不发送请求了
+  //   return request.respondWith(request, {
+  //       status: 404,
+  //       statusText: 'Not found'
+  //   });
 
-    //直接返回，不发送请求了
-    // return request.respondWith(request, {
-    //     status: 404,
-    //     statusText: 'Not found'
-    // });
-
-    next(function (response) {
-      // ...
-      // 请求发送后的处理逻辑
-      // ...
-      // 根据请求的状态，response参数会返回给successCallback或errorCallback
-      console.log('response-1111')
-      return response
-    })
-  });
-  Vue.http.interceptors.push(function (request, next) {
-    // ...
-    // 请求发送前的处理逻辑
-    // ...
-    console.log('request-22222')//request先走了这里
-    next(function (response) {
-      // ...
-      // 请求发送后的处理逻辑
-      // ...
-      // 根据请求的状态，response参数会返回给successCallback或errorCallback
-      console.log('response-22222')
-      return response
-    })
-  })
-}
-  /** http 设置全局配置 **/
-
-// const http = function ($http) {
-//   /** http 设置全局配置 **/
-
-//   //设置请求域
-//   // Vue.http.options.root = 'http://127.0.0.1:8081/';
-//   $http.options.root = '';
-//   //如果请求是基于这个域
-//   //错误: this.$http.get("/list.json"），不然是基于页面域名的请求
-//   //正确： this.$http.get("list.json"）
-
-//   //设置请求头
-//   $http.headers.common['Authorization'] = 'Basic YXBpOnBhc3N3b3Jk';
-
-//   //Vue组件内设置默认值选项。
-//   // new Vue({
-//   //     http: {
-//   //         root: '/root',
-//   //         headers: {
-//   //             Authorization: 'Basic YXBpOnBhc3N3b3Jk'
-//   //         }
-//   //     }
-//   // })
-
-//   /** 拦截器 **/
-//   $http.interceptors.push(function (request, next) {
-//     // ...
-//     // 请求发送前的处理逻辑
-//     // ...
-//     $http.$loading.show({
-//         show: true,
-//         msg: "fsa"
-//       });
-//     console.log('request-1111')
-
-//     // modify method
-//     request.method = 'POST';
-//     // modify headers
-//     request.headers.set('X-CSRF-TOKEN', 'TOKEN');
-
-//     //直接返回，不发送请求了
-//     // return request.respondWith(request, {
-//     //     status: 404,
-//     //     statusText: 'Not found'
-//     // });
-
-//     next(function (response) {
-//       // ...
-//       // 请求发送后的处理逻辑
-//       // ...
-//       // 根据请求的状态，response参数会返回给successCallback或errorCallback
-//       console.log('response-1111')
-//       return response
-//     })
-//   });
-//   $http.interceptors.push(function (request, next) {
-//     // ...
-//     // 请求发送前的处理逻辑
-//     // ...
-//     console.log('request-22222')//request先走了这里
-//     next(function (response) {
-//       // ...
-//       // 请求发送后的处理逻辑
-//       // ...
-//       // 根据请求的状态，response参数会返回给successCallback或errorCallback
-//       console.log('response-22222')
-//       return response
-//     })
-//   })
-//   /** http 设置全局配置 **/
-// }
-
-export default interceptor;
+  //   next(function (response) {
+  //     // ...
+  //     // 请求发送后的处理逻辑
+  //     // ...
+  //     // 根据请求的状态，response参数会返回给successCallback或errorCallback
+  //     console.log('response-1111')
+  //     return response
+  //   })
+  // });
